7312d91fadd7e762c7defc02860dc4fc98566e84
Emily Bache <emily.bache@pagero.com>
2016-06-09 21:45:07 +0200

    try to make the starting position clearer, whether you use gmock or not

--- cpp/DeviceDriverTest.cpp
+++ cpp/DeviceDriverTest.cpp
@@ -2,24 +2,23 @@
 #include "GMockMemoryDevice.hpp"
 #include "MockMemoryDevice.hpp"
 #include <gtest/gtest.h>
-
-// remove this line if you're not using gmock
 #include <gmock/gmock.h>
 
 using namespace ::testing;
 
+// You have the choice of using GMock or hand coding your mock. Delete one of these two tests depending on your choice.
+
 TEST(DeviceDriver, read)
 {
-    MockMemoryDevice hardware;
+    GMockMemoryDevice hardware;
+    EXPECT_CALL(hardware, read(_)).WillOnce(Return(0));
     DeviceDriver device(hardware);
     ASSERT_EQ(0, device.read(0xFF));
 }
 
-// remove this test if you're not using gmock
-TEST(DeviceDriver, read_using_gmock)
+TEST(DeviceDriver, read1)
 {
-    GMockMemoryDevice hardware;
-    EXPECT_CALL(hardware, read(_)).WillOnce(Return(0));
+    MockMemoryDevice hardware;
     DeviceDriver device(hardware);
     ASSERT_EQ(0, device.read(0xFF));
 }
