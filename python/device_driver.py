a336ef94c6b3f2d584bcc932a945adc5ee0b7123
Peter Kofler <peter.kofler@code-cop.org>
2019-05-06 17:50:11 +0200

    Python - format code, add type annotations.

--- python/device_driver.py
+++ python/device_driver.py
@@ -1,18 +1,18 @@
-
-from hardware_interface import FlashMemoryDevice
-
 class DeviceDriver:
     """
     This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
     """
 
     def __init__(self, device):
+        """
+        :type device: hardware_interface.FlashMemoryDevice
+        """
         self.device = device
 
     def write(self, address, data):
-        #TODO: implement this method
+        # TODO: implement this method
         pass
 
     def read(self, address):
-        #TODO: implement this method
+        # TODO: implement this method
         pass
--- python/hardware_interface.py
+++ python/hardware_interface.py
@@ -1,4 +1,3 @@
-
 class FlashMemoryDevice:
     """
     This class represents the interface to a Flash Memory Device. The hardware has only two methods - 'read' and 'write'
@@ -8,13 +7,17 @@ class FlashMemoryDevice:
     def read(self, address):
         """
         returns the data found at the address given.
-        This method is implemented in the actual hardware, this is just a stub showing the interface that method fulfills.
+        This method is implemented in the actual hardware, this is just a stub showing the interface that method fulfils.
+        :type address: int
+        :rtype: int
         """
         pass
 
     def write(self, address, data):
         """
         write the data given to the address given.
-        This method is implemented in the actual hardware, this is just a stub showing the interface that method fulfills.
+        This method is implemented in the actual hardware, this is just a stub showing the interface that method fulfils.
+        :type address: int
+        :type data: int
         """
         pass
