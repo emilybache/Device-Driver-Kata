1e51a5438e01fe391b10cc9397375c43eda19ea8
Olivier PEREZ <olivier@olivierperez.fr>
2021-07-08 14:29:09 +0200

    Kotlin - Add new language

---
+++ Kotlin/src/main/kotlin/codekata/DeviceDriver.kt
@@ -0,0 +1,14 @@
+package codekata
+
+class DeviceDriver(hardware: FlashMemoryDevice) {
+
+    fun read(address: Long): Byte {
+        // TODO: implement this method
+        return -1
+    }
+
+    fun write(address: Long, data: Byte) {
+        // TODO: implement this method
+    }
+
+}
