b84f649b5a54d6a64bdd82497171dbc26a2ee92e
Sergey Kudashev <kudashevs@gmail.com>
2024-02-17 22:24:04 +0300

    Update FlashMemoryDevice update class docblock

--- php/src/CodeKata/FlashMemoryDevice.php
+++ php/src/CodeKata/FlashMemoryDevice.php
@@ -6,9 +6,7 @@ namespace CodeKata;
 
 /**
  * This class represents the interface to a Flash Memory Device. The hardware has only two methods - 'read' and 'write'
- * However, the interface for using the device is a lot more complex than that.
- * It is outlined in the top-level README file.
- * @package CodeKata
+ * However, the interface for using the device is a lot more complex than that. It is outlined in the top-level README file.
  */
 interface FlashMemoryDevice
 {
